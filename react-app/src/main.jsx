9bb295fe2cceffddbe5d36ffcc96c37deb6e7bbd
tommasodiviesti <tommaso.diviesti@studenti.unipd.it>
2025-03-15 22:16:26 +0100

    altre prove react

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './css/style.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
+
+import store from "./redux/store"
+import { Provider} from "react-redux"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </StrictMode>,
 )
