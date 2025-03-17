3007d9bddb881e103c0063d8e544015cb39b4671
tommasodiviesti <tommaso.diviesti@studenti.unipd.it>
2025-03-17 22:06:20 +0100

    prove material

--- react-app/src/pages/login.jsx
+++ react-app/src/pages/login.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import '../css/style.css'
+import {MainView} from "../components/MainView"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <MainView></MainView>
       <p>Ce l'abbiamo fatta!</p>
     </>
   )
