afa6af9ba12044ecec0380cebe8237d17a626ac1
Gabriele Di Pietro <gabrielecorubolo@gmail.com>
2025-03-18 22:21:33 +0100

    update

--- react-app/src/components/chatbot.jsx
+++ react-app/src/components/chatbot.jsx
@@ -1,12 +1,30 @@
 import { useState } from 'react';
+import '../css/chat.css';
 
 function Chatbot() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <p>Lesss goooo - conteggio: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increase Count</button>
+      <div className="Chat">
+      <div className="Top">
+        <div className="user">
+          <img src="#" alt="icona di giorgione"/>
+          <div className="texts">
+            <span>Giorgione</span>
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="Bottom">
+        <div className="icons">
+          <img src="#" alt="icona di allegato"/>
+        </div>
+        <input type="text" placeholder="Scrivi un messaggio..."/>
+        <button className="sendButton">Invia</button>
+      </div>
+      </div>
     </>
   );
 }
