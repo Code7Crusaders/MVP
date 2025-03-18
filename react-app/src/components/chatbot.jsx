cadad50864e4ce821100b1da9f008f11d51dc7c3
Gabriele Di Pietro <gabrielecorubolo@gmail.com>
2025-03-18 22:43:34 +0100

    fine chat

--- react-app/src/components/chatbot.jsx
+++ react-app/src/components/chatbot.jsx
@@ -1,8 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../css/chat.css';
 
 function Chatbot() {
   const [count, setCount] = useState(0);
+
+
+  const endRef = useRef(null);
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [])
 
   return (
     <>
@@ -16,7 +22,22 @@ function Chatbot() {
           </div>
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message"> // messaggio di giorgione
+          <img src="#" alt="icona di giorgione"/>
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own"> // messaggio dell'utente
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
+      </div>
       <div className="Bottom">
         <div className="icons">
           <img src="#" alt="icona di allegato"/>
