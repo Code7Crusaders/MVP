cba776e5c50e2adcbd40eb6865977f131c28fcc9
tommasodiviesti <tommaso.diviesti@studenti.unipd.it>
2025-03-19 13:33:26 +0100

    aggiornamenti chatbot

--- react-app/src/components/chatbot.jsx
+++ react-app/src/components/chatbot.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import '../css/chat.css';
 
 function Chatbot() {
@@ -22,15 +22,23 @@ function Chatbot() {
           </div>
         </div>
       </div>
+    
       <div className="center">
-        <div className="message"> // messaggio di giorgione
-          <img src="#" alt="icona di giorgione"/>
+        <div className="message"> 
+          <img src="https://avatars.githubusercontent.com/u/185105956?s=200&v=4" alt="icona di giorgione"/>
           <div className="texts">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Ciao User, come posso esserti utile?</p>
             <span>1 min ago</span>
           </div>
         </div>
-        <div className="message own"> // messaggio dell'utente
+        <div className="message own"> 
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message"> 
+          <img src="https://avatars.githubusercontent.com/u/185105956?s=200&v=4" alt="icona di giorgione"/>
           <div className="texts">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <span>1 min ago</span>
