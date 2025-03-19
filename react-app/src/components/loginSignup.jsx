9b7605f8068561e7458b208e23961e4f3b52fe0b
SerpenTaki <gabrielecorubolo@gmail.com>
2025-03-19 17:19:16 +0100

    creati login ovviamente sono da fare ma almeno ho una base

---
+++ react-app/src/components/loginSignup.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState, useRef } from 'react';
+import '../css/loginSignup.css';
+
+import userIcon from '../assets/user.svg';
+import passwordIcon from '../assets/password.svg';
+import emailIcon from '../assets/email.svg';
+
+const loginSignup = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default loginSignup;
