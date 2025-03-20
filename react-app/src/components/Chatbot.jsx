050372920d5432d3f97633fcb689807b4ac83da8
tommasodiviesti <tommaso.diviesti@studenti.unipd.it>
2025-03-20 17:08:58 +0100

    altri fix

--- react-app/src/components/Chatbot.jsx
+++ react-app/src/components/Chatbot.jsx
@@ -14,9 +14,15 @@ function Chatbot() {
   const theme = useTheme();
   
   const inputChatStyle = {
-    backgroundColor: theme.palette.mode === 'dark' ? 'rgba(17, 25, 40, 0.9)' : '#e6e6e6',
-    color: theme.palette.mode === 'dark' ? 'white' : 'black', 
+    backgroundColor: theme.palette.mode === 'dark' ? 'rgba(17, 25, 40, 0.9)' : '#ededed',
+    color: theme.palette.mode === 'dark' ? 'white' : 'black',
+            '::placeholder': {
+              color: theme.palette.mode === 'dark' ? 'lightgray' : 'gray',} 
   };
+
+  const timeSpan = {
+    color: theme.palette.mode === 'dark' ? 'white' : 'black',
+  }
 
   const endRef = useRef(null);
   useEffect(() => {
@@ -31,10 +37,10 @@ function Chatbot() {
           <p>Prova di un titolo per questa chat</p>
         </div>
         <div className='icons' >
-          <img src={addIcon} className='btnsTop' alt="aggiungi un template"/>
-          <img src={answIcon} className='btnsTop' alt="seleziona template"/>
-          <img src={saveIcon} className='btnsTop' alt="Salva la chat"/>
-          <img src={deleteIcon} className='btnsTop' alt="Elimina la chat"/>
+          <img src={addIcon} className='btnsTop' alt="aggiungi un template" title='Aggiungi Template'/>
+          <img src={answIcon} className='btnsTop' alt="seleziona template" title='Seleziona domanda'/>
+          <img src={saveIcon} className='btnsTop' alt="Salva la chat" title='Salva Chat'/>
+          <img src={deleteIcon} className='btnsTop' alt="Elimina la chat" title='Elimina Chat'/>
         </div>
       </div>
     
@@ -43,33 +49,26 @@ function Chatbot() {
           <img src={MuccaIcon} alt="icona di giorgione"/>
           <div className="texts">
             <p>Ciao User, come posso esserti utile?</p>
-            <span>1 min ago</span>
+            <span style={timeSpan}>1 min ago</span>
           </div>
         </div>
         <div className="message own"> 
           <div className="texts">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            <span>1 min ago</span>
+            <span style={timeSpan}>1 min ago</span>
           </div>
         </div>
         <div className="message"> 
           <img src={MuccaIcon} alt="icona di giorgione"/>
           <div className="texts">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <span>1 min ago</span>
+            <span style={timeSpan}>1 min ago</span>
           </div>
         </div>
         <div ref={endRef}></div>
       </div>
       <div className="bottom">
-      <input  type="text"  placeholder="Scrivi un messaggio..." 
-        style={{
-            backgroundColor: theme.palette.mode === 'dark' ? 'rgba(17, 25, 40, 0.9)' : '#e6e6e6',
-            color: theme.palette.mode === 'dark' ? 'white' : 'black',
-            '::placeholder': {
-              color: theme.palette.mode === 'dark' ? 'lightgray' : 'gray'
-            }
-        }}/>
+      <input  type="text"  placeholder="Scrivi un messaggio..." style={inputChatStyle} />
         <button className='sendButton'>Invia</button>
       </div>
       </div>
