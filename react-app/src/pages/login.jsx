7eeeb3ea67d9794236d3059d51a2ecc9af331330
tommasodiviesti <tommaso.diviesti@studenti.unipd.it>
2025-03-26 22:18:51 +0100

    Update login.jsx

--- react-app/src/pages/login.jsx
+++ react-app/src/pages/login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import '../css/login.css'
 
 const Login = () => {
   const [username, setUsername] = useState('');
